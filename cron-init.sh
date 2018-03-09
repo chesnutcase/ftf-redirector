c737f5064824ed581581929d31dc9789a7c30c3d
Chester Koh <chester@detrack.com>
2018-03-10 00:56:15 +0800

    attempt to automate installation of cronfile

--- cron-init.sh
+++ cron-init.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-croncmd="php $(pwd)/cronjob.php"
-cronjob="0 */15 * * * $croncmd"
+#still doesn't work - you'll have to manually add the cronjob yourself
+#if you can point out what's wrong, please tell me!
+croncmd="php $(pwd)/cronjob.php > $(pwd)/cronlog.txt"
+cronjob="*/15 * * * * $croncmd"
 echo $croncmd
 if [ "$1" == "--remove" ]
 then
-  echo "removing"
+  echo "removing cronjob";
+  echo $(crontab -l | grep -v $croncmd) | crontab -
 else
   echo "adding cronjob"
+  if [[ $(crontab -l | egrep -v "^(#|$)" | egrep -v "^no crontab for" | grep -q $croncmd; echo $?) == 1 ]]
+  then
+    echo $(crontab -l | egrep -v "^no crontab for" ; echo $croncmd) | crontab -
+  fi
 fi
